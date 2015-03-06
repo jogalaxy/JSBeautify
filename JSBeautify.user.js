dc2a16c4d1ce7b93eb21ba6c87ef20d863b9617f
jogalaxy <jo123456789@gmx.fr>
2015-03-06 18:43:21 +0100

    Correction bug

--- JSBeautify.user.js
+++ JSBeautify.user.js
@@ -3,22 +3,32 @@
 // @namespace    Editor
 // @downloadURL  https://raw.githubusercontent.com/jogalaxy/JSBeautify/master/JSBeautify.user.js
 // @updateURL    https://raw.githubusercontent.com/jogalaxy/JSBeautify/master/JSBeautify.user.js
-// @version      0.1
+// @version      0.2
 // @description  Améliore la refonte du code de l'éditeur
 // @author       jojo123
 // @match        http://leekwars.com/editor
 // @grant        none
 // ==/UserScript==
 
-var js_beautify_back = js_beautify;
+function embed() {
+	var oldFunc = window.js_beautify;
 
-js_beautify = function(sourceCode)
-{
-    return js_beautify_back(sourceCode, {
-        'indent_size': 1,
-        'indent_char': '\t',
-        'jslint_happy' : true,
-        'brace_style' : 'expand',
-        'space_before_conditional' : true, // false : pour supprimer l'espace entre "if" et la condition => if(condition)
-    });
+	window.js_beautify = function(sourceCode)
+	{
+		return oldFunc(sourceCode, {
+			'indent_size': 1,
+			'indent_char': '\t',
+			'jslint_happy' : true,
+			'brace_style' : 'expand',
+			'space_before_conditional' : true, // false : pour supprimer l'espace entre "if" et la condition => if(condition)
+		});
+	};
+
 }
+
+var inject = document.createElement("script");
+
+inject.setAttribute("type", "text/javascript");
+inject.appendChild(document.createTextNode("(" + embed + ")()"));
+
+document.body.appendChild(inject);
